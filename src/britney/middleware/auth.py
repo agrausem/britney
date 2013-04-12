e6b43d06feb8aadf54d04674a5ef8b0a9a131996
Arnaud Grausem <arnaud.grausem@unistra.fr>
2013-04-12 17:14:44 +0200

    refactoring auth base middleware

--- src/britney/middleware/auth.py
+++ src/britney/middleware/auth.py
@@ -30,11 +30,8 @@ class Auth(Middleware):
     def __call__(self, environ):
         """
         """
-        if hasattr(self, 'process_request') and self.needs_auth(environ):
-            self.process_request(environ)
-        if hasattr(self, 'process_response'):
-            callback = partial(self.process_response)
-            return callback
+        if self.needs_auth(environ):
+            super(Auth, self).__call__(environ)
 
 
 class Basic(Auth):
