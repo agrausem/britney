8a0f7dad21f99dc1c804774803b5bd3a98376364
Arnaud Grausem <arnaud.grausem@unistra.fr>
2013-09-15 22:21:14 +0200

    add tests for request headers and data

--- tests/test_request.py
+++ tests/test_request.py
@@ -8,8 +8,6 @@ from britney.request import RequestBuilder
 
 
 class TestRequestBuilderUrl(unittest.TestCase):
-    """
-    """
 
     def setUp(self):
         path = os.path.join(os.path.dirname(__file__), 'data', 'request.json')
@@ -17,13 +15,24 @@ class TestRequestBuilderUrl(unittest.TestCase):
             self.data = json.loads(method_data.read())
 
     def test_good_url(self):
-        """
-        """
         for test_data in self.data:
             built_request = RequestBuilder(test_data['env'])
-            """
-            for property_ in ('netloc', 'base_url', 'path'):
-                self.assertEqual(getattr(property_, built_request),
-                        test_data[property_])
-            """
             self.assertEqual(built_request.uri, test_data['url'])
+
+    def test_headers(self):
+        environ = {'spore.headers': [('Authorization', 'cdbcdvvvfvf=='),
+            ('Content-Type', 'text/plain')]}
+        environ.update(self.data[0]['env'])
+        built_request = RequestBuilder(environ)
+        headers = built_request.headers()
+        self.assertIn('Authorization', headers)
+        self.assertIn('Content-Type', headers)
+        self.assertEqual(headers['Authorization'], 'cdbcdvvvfvf==')
+        self.assertEqual(headers['Content-Type'], 'text/plain')
+
+    def test_payload(self):
+        environ = {'spore.payload': {'param': 'test'}}
+        environ.update(self.data[0]['env'])
+        built_request = RequestBuilder(environ)
+        payload = built_request.data()
+        self.assertEqual(payload, {'param': 'test'})
