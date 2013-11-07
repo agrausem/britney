4b8e278284bfb9dd335394ce442f5aa29e451529
Arnaud Grausem <arnaud.grausem@unistra.fr>
2013-11-07 09:11:57 +0100

    new version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('requirements.txt') as requirements:
 
 setup(
     name='britney',
-    version='0.1',
+    version='0.2',
     author='Arnaud Grausem',
     author_email='arnaud.grausem@gmail.com',
     maintainer='Arnaud Grausem',
