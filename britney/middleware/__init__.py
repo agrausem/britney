f7cd628dfa8e4a7f8caee74a1eea0e3f1133748c
Arnaud Grausem <arnaud.grausem@gmail.com>
2014-04-29 19:21:33 +0200

    entry points from 'britney.plugins.middleware' group are now loaded on top of britney.middleware package

--- britney/middleware/__init__.py
+++ britney/middleware/__init__.py
@@ -10,3 +10,11 @@ britney.middleware
 
 from .auth import *
 from .format import *
+
+from pkg_resources import iter_entry_points
+
+for entry_point in iter_entry_points('britney.plugins.middleware'):
+    try:
+        locals()[entry_point.name] = entry_point.load()
+    except ImportError:
+        pass
